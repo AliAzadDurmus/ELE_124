399398d4613d2d9af2188b865b2f1f051d64ffc2
AliAzadDurmus <aliazaddurmus06@gmail.com>
2025-05-07 17:00:06 +0300

    Exercise3.c

--- HW2/Exercise3.c
+++ HW2/Exercise3.c
@@ -12,22 +12,28 @@ int main(void)
 	srand(time(0));
 	FILE *fptr;
 	
-	fptr = fopen("donations.txt","w");
+	fptr = fopen("donations.txt","a+");
 	
 	for( int i=0; i<50; i++ )
 	{ 
-		itemPrice = rand()%1999 + 1;
+		itemPrice = rand()%2001;
 		itemPrice = itemPrice / 100;
-		printf("Item is %.2f\n", itemPrice);
-		fprintf(fptr, "Item is %.2f\n", itemPrice);
+		printf("%2d)Item is %8.2f$\n", i+1, itemPrice);
+		printf("   Donation is %3.2f$\n", itemPrice - floor(itemPrice));
+		printf("   Profit is %6.2f$\n\n", floor(itemPrice));
 		
 		Hunerler(itemPrice, &donationTotal, &profitTotal);
+		
+		fprintf(fptr, "%2d)Item is %8.2f$\n", i+1, itemPrice);
+		fprintf(fptr, "   Donation is %3.2f$\n", itemPrice - floor(itemPrice));
+		fprintf(fptr, "   Profit is %6.2f$\n\n", floor(itemPrice));
+		
 	}
-	
-	printf("Donation is %.2f\n", donationTotal);
-	printf("Profit is %.2f\n", profitTotal);
-	fprintf(fptr, "Donation is %.2f\n", donationTotal);
-	fprintf(fptr,"Profit is %.2f\n", profitTotal);
+		
+	printf(" --------------------\n   Donation is %.2f$\n", donationTotal);
+	printf("   Profit is %8.2f$\n --------------------\n", profitTotal);
+	fprintf(fptr, " --------------------\n   Donation is %.2f$\n", donationTotal);
+	fprintf(fptr, "   Profit is %8.2f$\n --------------------\n", profitTotal);
 	
 	fclose(fptr);
 	
@@ -38,4 +44,4 @@ void Hunerler(float itemPrice, float* donationTotal, float* profitTotal)
 {
 	*donationTotal += itemPrice - (int)itemPrice;
 	*profitTotal += (int)itemPrice;
-} 
+}
